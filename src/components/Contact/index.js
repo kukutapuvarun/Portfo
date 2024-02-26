20f44cae16b7a36e0810f4c0a0343357261fec80
kukutapuvarun <kukutapuvarun2003@gmail.com>
2024-02-26 20:40:50 +0530

    edited

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Loader from 'react-loaders'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useRef } from 'react'
 import emailjs from '@emailjs/browser'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Contact = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass] = useState('text-animate')
   const form = useRef()
 
 
@@ -19,7 +18,7 @@ const Contact = () => {
       .then(
         () => {
           alert('Message successfully sent!')
-          window.location.reload(false)
+          window.location.reload(true)
         },
         () => {
           alert('Failed to send the message, please try again')
